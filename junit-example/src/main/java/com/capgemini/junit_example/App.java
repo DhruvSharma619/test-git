5254a6a96cc5219428abca3aca1fe7351be7277a
DhruvSharma619 <dhruv6109@gmail.com>
2025-05-12 12:28:16 +0530

    first

---
+++ junit-example/src/main/java/com/capgemini/junit_example/App.java
@@ -0,0 +1,14 @@
+package com.capgemini.junit_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "hi dhruv sharma " );
+    }
+}
